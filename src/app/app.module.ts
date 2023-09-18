3e02b349cb787c22de395dc41d0c0cebd589ade8
Benjamin <benjaschell@gmail.com>
2023-09-17 21:51:17 -0300

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { AboutComponent } from './page/about/about.component';
+import { AhorcadoComponent } from './page/juegos/ahorcado/ahorcado.component';
+import { MayoromenorComponent } from './page/juegos/mayoromenor/mayoromenor.component';
+import { PreguntadosComponent } from './page/juegos/preguntados/preguntados.component';
+import { MijuegoComponent } from './page/juegos/mijuego/mijuego.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    AhorcadoComponent,
+    MayoromenorComponent,
+    PreguntadosComponent,
+    MijuegoComponent
   ],
   imports: [
     BrowserModule,
