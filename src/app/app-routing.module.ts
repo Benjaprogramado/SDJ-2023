8650d94ce0618464855544b8e06d8cc77bb48545
Benjamin <benjaschell@gmail.com>
2023-09-18 00:50:02 -0300

    laisyLoadJuegos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './page/error/error.component';
+import { LoginComponent } from './page/login/login.component';
+import { AboutComponent } from './page/about/about.component';
+import { HomeComponent } from './page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch:'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component:AboutComponent },
+  { path: 'home', component:HomeComponent },
+  {
+    path: 'juegos',
+    loadChildren: () => import('../app/page/juegos/juegos.module').then(m => m.JuegosModule)
+  },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/page/juegos/juegos.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JuegosRoutingModule } from './juegos-routing.module';
+import { JuegosComponent } from './juegos.component';
+
+
+@NgModule({
+  declarations: [
+    JuegosComponent
+  ],
+  imports: [
+    CommonModule,
+    JuegosRoutingModule
+  ],
+  exports: [
+    JuegosComponent
+  ],
+})
+export class JuegosModule { }
